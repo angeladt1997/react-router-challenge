20b4f3c929091f624f3aa062caff8b519c6244a0
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-08-27 17:48:31 -0500

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Roueter} from 'react-dom-router';
 import AboutMenu from './about-menu';
 
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            <Router path='/aboutAboutMenu />
         </div>
     );
 }
